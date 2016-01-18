d4d3dafd139ca9104edd0584508b45e3db918842
Pierre-Henri Gache <pierrehenri.gache@outlook.com>
2016-01-18 11:44:25 +0100

    Update resources json & add init data script

---
+++ MusicStore/MvcMusicStore.Sql.DefaultConnection/Scripts/InitData.sql
@@ -0,0 +1,40 @@
+﻿
+/*
+This script was created by Visual Studio on 18/01/2016 at 11:23.
+Run this script on musicstoredatabaseserver.database.windows.net.DefaultConnection (sqladmin) to make it the same as localhost.DefaultConnection (DESKTOP-FQRK793\pierr).
+This script performs its actions in the following order:
+1. Disable foreign-key constraints.
+2. Perform DELETE commands. 
+3. Perform UPDATE commands.
+4. Perform INSERT commands.
+5. Re-enable foreign-key constraints.
+Please back up your target database before running this script.
+*/
+SET NUMERIC_ROUNDABORT OFF
+GO
+
+SET XACT_ABORT, ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+
+/*Pointer used for text / image updates. This might not be needed, but is declared here just in case*/
+DECLARE @pv binary(16)
+BEGIN TRANSACTION
+ALTER TABLE [dbo].[AspNetUserRoles] DROP CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId]
+ALTER TABLE [dbo].[AspNetUserRoles] DROP CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId]
+ALTER TABLE [dbo].[AspNetUserLogins] DROP CONSTRAINT [FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId]
+ALTER TABLE [dbo].[AspNetUserClaims] DROP CONSTRAINT [FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId]
+INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'3d3d7842-d986-4e56-8a09-e982ff8c2124', NULL, 0, N'AAWjtR9YjmFWLVtvSZQWJCSw5gJ1BhDcWUzi35ZgOW4uIefWAUmvUY1rpXsk19BJ7w==', N'9027531d-7621-417f-9b6e-e75b6ac3be24', NULL, 0, 0, NULL, 0, 0, N'Administrator')
+INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'3d3d7842-d986-4e56-8a09-e982ff8c2124', N'f1df49ee-353f-4846-8c70-eb18e73cfa98')
+INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'f1df49ee-353f-4846-8c70-eb18e73cfa98', N'Administrator')
+ALTER TABLE [dbo].[AspNetUserRoles]
+    ADD CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+ALTER TABLE [dbo].[AspNetUserRoles]
+    ADD CONSTRAINT [FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[AspNetRoles] ([Id]) ON DELETE CASCADE
+ALTER TABLE [dbo].[AspNetUserLogins]
+    ADD CONSTRAINT [FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+ALTER TABLE [dbo].[AspNetUserClaims]
+    ADD CONSTRAINT [FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+COMMIT TRANSACTION
+
+
+GO
