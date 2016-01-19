a49ac07bc82c8395d9d3603481949fa8a754761f
Pierre-Henri Gache <pierrehenri.gache@outlook.com>
2016-01-19 09:12:50 +0100

    Add build tools

---
+++ LoadTests/BuildTools/FlattenOutput.ps1
@@ -0,0 +1,22 @@
+﻿#
+# FlattenOutput.ps1
+#
+
+param
+(
+    [string]$Folder
+)
+
+foreach($item in Get-ChildItem $Folder -Recurse -Directory)
+{  
+    $path = Join-Path $Folder $item
+    $pathwildcard = $path + "\*"
+
+    Write-Output "Flatten $path"
+
+    Move-Item $pathwildcard $Folder -Force
+
+    Remove-Item $path
+}
+
+Get-ChildItem $Folder -Recurse | FT Name, Directory
